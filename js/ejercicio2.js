a69a562a47137d9f42b4ea1d7d290f5a28da0285
Aleexfdez <alexfernandez567@gmail.com>
2023-10-02 13:26:45 +0200

    ejercicio finalizado

---
+++ js/ejercicio2.js
@@ -0,0 +1,11 @@
+var numeroX = parseFloat(prompt("Introduce un numero X:"));
+var numeroY = parseFloat(prompt("Introduce un numero Y:"));
+
+console.log("El número X redondeado a la alta es: " + Math.ceil(numeroX));
+console.log("El número Y redondeado a la baja es: " + Math.floor(numeroY));
+console.log("El entero más próximo al número Y es: " + Math.round(numeroY));
+console.log("Un número aleatorio entre 0 y 1 es: " + Math.random());
+console.log("El valor del número X elevando al número Y: " + Math.pow(numeroX, numeroY));
+console.log("La raiz cuadrada del número Y: " + Math.sqrt(numeroY));
+console.log("El máximo entre el número X y el número Y es: " + Math.max(numeroX, numeroY));
+console.log("El mñinimo entre el número X y el número Y es: " + Math.min(numeroX, numeroY));
